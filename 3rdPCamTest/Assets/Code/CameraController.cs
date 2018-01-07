1c7e451d54ba5c6aec6199c2394e6555e64ba109
Johan-1 <nilssonjohan10@gmail.com>
2018-01-07 22:13:49 +0100

    finish camera collision and add lerp back after collision

--- 3rdPCamTest/Assets/Code/CameraController.cs
+++ 3rdPCamTest/Assets/Code/CameraController.cs
@@ -8,105 +8,103 @@ public class CameraController : MonoBehaviour
     [SerializeField] Transform _target;
     [SerializeField] Vector2 _cameraRotationXMinMax = new Vector2(-10, 10);
 
-    [SerializeField] float _cameraDistance = 1.0f;
+    [SerializeField] float _cameraDistanceMax = 1.0f;
     [SerializeField] float _mouseSensitivityY = 2.0f;
     [SerializeField] float _mouseSensitivityX = 0.2f;
+    [SerializeField] float _lerpSpeed = 0.5f;
 
     [SerializeField] Vector2 _stickSensitivity;
 
-    float _usingDistance;
+    float _targetCameraDistance;
 
+    
+   
+    float _lastRayHitPosition = 0.0f;
+    float _lerpOutFraction = 0.0f;
+
+    
     Vector3 _cameraRotation = new Vector3(0, 180, 0);
 
     void Awake()
     {
-        _usingDistance = _cameraDistance;    
+        _targetCameraDistance = _cameraDistanceMax;
+        _lastRayHitPosition = _cameraDistanceMax;                       
     }
 
     void LateUpdate()
     {
-
-
-
-
-        
-
-        CheckCollision();
-        CheckInterseption();
-        MoveCamera();
-
-
-
+        MoveCamera();        
+        CheckInterseption();        
     }
 
     void CheckInterseption()
     {
 
-        Vector3 targetToCamera = (transform.position + new Vector3(0, -0.5f,0)) - _target.position;
-        targetToCamera.Normalize();
+        // cast 3 rays from player towards the camera        
+        Vector3[] targetToCameraFLR = new Vector3[3];
 
-        float extraDistance = 0.5f;
+        targetToCameraFLR[0] = (transform.position + new Vector3(0, -0.5f, 0)) - _target.position; // straight from below for floor collision
+        targetToCameraFLR[1] = (transform.position + transform.right * 0.35f) - _target.position;  // Left for wall collision
+        targetToCameraFLR[2] = (transform.position + -transform.right * 0.35f) - _target.position; // right for wall collision
 
+        // move the camera a little extra back from the hitpoint, this makes the camera backup smooth against the floor
+        float extraDistance = 0.01f;       
+        int hitCount = 0; // count how many rays that hit something
         RaycastHit hit;
 
-        if (Physics.Raycast(_target.position, targetToCamera, out hit, _usingDistance))
-        {           
-            transform.position = hit.transform.position + hit.normal * extraDistance;
-            
-            if(hit.distance + extraDistance < _cameraDistance)
-               _usingDistance = hit.distance + extraDistance;
-                        
-        }
-
-
-        if (!Physics.Raycast(_target.position, targetToCamera, out hit, _cameraDistance + 0.4f))
-        {            
-            _usingDistance = _cameraDistance;
-        }
-
-
-        
-        
-       
-        Debug.DrawRay(_target.position, targetToCamera * _usingDistance, Color.cyan);
-        
-
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawSphere(transform.position, 1.0f);
-    }
-
-    void CheckCollision()
-    {
-        if (Physics.CheckSphere(transform.position, 1.0f))
+        for (int i =0; i < 3; i++)
         {
-            Collider[] collisions = Physics.OverlapSphere(transform.position, 1.0f);
+            targetToCameraFLR[i].Normalize();
+            if (Physics.Raycast(_target.position, targetToCameraFLR[i], out hit, _targetCameraDistance))
+            {
+                // set the targetDistance to the lenght of ray from origin to hitpoint
+                // set the LastRayHitLenght to the same value. we will start our out lerp from this value if we dont hit anything the next frame
+                // set lerp fraction to be 0 if anything is hit so lerp will restart from beginning
+                _targetCameraDistance = hit.distance + extraDistance;
+                _lastRayHitPosition = _targetCameraDistance;
+                _lerpOutFraction = 0;
 
+                hitCount++;
+            }
+        }
 
+        // if no ray hit it is safe to start lerping back out to max distance of camera
+        if (hitCount == 0)
+        {
             
-            print("camera colliding");
-            
+            _lerpOutFraction += Time.deltaTime * _lerpSpeed;
+            _lerpOutFraction = Mathf.Clamp01(_lerpOutFraction);
+            _targetCameraDistance = Mathf.Lerp(_lastRayHitPosition, _cameraDistanceMax, _lerpOutFraction);
         }
         
+        // draw rays for debuging
+        Debug.DrawRay(_target.position, targetToCameraFLR[0] * _targetCameraDistance, Color.cyan);
+        Debug.DrawRay(_target.position, targetToCameraFLR[1] * _targetCameraDistance, Color.cyan);
+        Debug.DrawRay(_target.position, targetToCameraFLR[2] * _targetCameraDistance, Color.cyan);        
+
     }
+       
 
     void MoveCamera()
     {
-        //get y and x rotations and clamp to desired values, set the local rotation of shoulder camera
+        
+        // get mouse input and add to rotation
         _cameraRotation.y += Input.GetAxisRaw("Mouse X") * _mouseSensitivityY;
         _cameraRotation.x += Input.GetAxisRaw("Mouse Y") * _mouseSensitivityY;
 
+        // get right controllstick and add to rotation
         _cameraRotation.y += Input.GetAxisRaw("RightStickX") * _stickSensitivity.x * Time.deltaTime;
         _cameraRotation.x += Input.GetAxisRaw("RightStickY") * _stickSensitivity.y * Time.deltaTime;
 
-
+        // clamp the x rotation of camera to avoid flipping
         _cameraRotation.x = Mathf.Clamp(_cameraRotation.x, _cameraRotationXMinMax.x, _cameraRotationXMinMax.y);
 
-        transform.position = _target.position + Quaternion.Euler(_cameraRotation) * new Vector3(0, 0, _usingDistance);
+        // set the position to the position of target and add the rotation multiplied by distance to make camera orbit around player
+        transform.position = _target.position + Quaternion.Euler(_cameraRotation) * new Vector3(0, 0, _targetCameraDistance);
 
+        // last focus forward on player
         transform.LookAt(_target.transform);
+
     }
 
 }
--- 3rdPCamTest/Assets/Code/PlayerController.cs
+++ 3rdPCamTest/Assets/Code/PlayerController.cs
@@ -5,26 +5,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
-    Vector3 _direction;
-    Vector3 _rotation;
-    Rigidbody _rigidbody;
+        
     [SerializeField] float _moveSpeed = 5.0f;
-
-
-    [SerializeField] Camera _camera;
+    [SerializeField] Camera _camera;        
     
-
-    
-    
-
-    Vector3 _cameraRotation;
-
+    Rigidbody _rigidbody;
     Animator _animator;
 
 	void Start () 			
 	{
-        _rigidbody = GetComponent<Rigidbody>();
-        
+        _rigidbody = GetComponent<Rigidbody>();        
         _animator = GetComponent<Animator>();
 	}
 
@@ -33,46 +23,35 @@ public class PlayerController : MonoBehaviour
 	void Update () 	
 	{
         SetVelocity();
-        SetRotations();
-       
-        
-
-        
-        
+                                      
     }
 
     void SetVelocity()
     {
-
-        // get forward direction based on camera
+        // get forward direction based on camera without the hight difference (dont want a forward leaning downwards/upwards)
         Vector3 camToPlayer = transform.position - new Vector3(_camera.transform.position.x, transform.position.y, _camera.transform.position.z);
         camToPlayer.Normalize();
 
-        //movement
-        _direction = _camera.transform.right * Input.GetAxisRaw("Horizontal") + camToPlayer  * Input.GetAxisRaw("Vertical");
-        _direction.Normalize();
-        _rigidbody.velocity = new Vector3(_direction.x * _moveSpeed, _rigidbody.velocity.y, _direction.z * _moveSpeed);
-
+        // get input direction based on the view from camera
+        Vector3 direction = ((_camera.transform.right * Input.GetAxisRaw("Horizontal")) + (camToPlayer  * Input.GetAxisRaw("Vertical"))).normalized;
         
-
-        // animation and forward facing
+        // set velocity keeping gravity
+        _rigidbody.velocity = new Vector3(direction.x * _moveSpeed, _rigidbody.velocity.y, direction.z * _moveSpeed);
+        
+        // set forward of model to direction of input
+        // only set if we have input to avoid rotation snapping back when standing still
         float moving = 0.0f;
-        if (_direction != Vector3.zero)
+        if (direction != Vector3.zero)
         {
             moving = 1.0f;
-            transform.forward = _direction;
-
-        }            
+            transform.forward = direction;
+        }     
+        
         _animator.SetFloat("velocity", moving);
 
     }
 
-    void SetRotations()
-    {
-
-
-
-    }
+    
 
 
 
